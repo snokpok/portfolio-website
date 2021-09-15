9ccf09f4b3667e9b519c9f6c7cec07c824bb765f
Vincent Vu <vinceny.fe@gmail.com>
2021-09-15 12:01:45 -0700

    added koseur in projects

--- js/projectList.js
+++ js/projectList.js
@@ -1,36 +1,42 @@
 const projectsList = [
   {
-    name: "Heart Attack Classification",
-    description: "Project taken from a freelancing client",
-    link: "https://github.com/Snakpi/heart-attack-clf/",
-    key: "hat",
-    imgFormat: "jpg",
-  },
-  {
-    name: "EDA on Wish's Summer Clothes Dataset",
-    description:
-      "Quantile analysis on Wish's Summer Clothes Dataset' on Kaggle",
-    link:
-      "https://www.kaggle.com/snakepie/eda-of-wish-s-summer-clothes-sales-data",
-    key: "wsc",
-    imgFormat: "jpg",
+    name: "Koseur: Nightlife App",
+    description: "Nightlife app for Vietnamese bars, built with Next.js + Sass",
+    link: "https://koseur-frontend.vercel.app/",
+    key: "koseur",
+    imgFormat: "png",
   },
   {
     name: "TDS Company Ltd. Landing Page",
     description:
       "Renewing a Vietnamese ceramics company's landing page with HTML5 + CSS3",
-    link: "https://tdsco-ceramic.com",
+    link: "http://tdsco-ceramic.com",
     key: "tds",
     imgFormat: "jpg",
   },
   {
     name: "Simple chat app",
     description:
-      "Chat app to experiment with Firebase's Firestore and OAuth services. Built with NextJS + ChakraUI",
+      "Chat app to experiment with Firebase's Firestore and OAuth services. Built with Next.js + ChakraUI",
     link: "https://chat-app.vuvincent.com",
     key: "chat-app",
     imgFormat: "png",
   },
+  // {
+  //   name: "Heart Attack Classification",
+  //   description: "Project taken from a freelancing client",
+  //   link: "https://github.com/Snakpi/heart-attack-clf/",
+  //   key: "hat",
+  //   imgFormat: "jpg",
+  // },
+  // {
+  //   name: "EDA on Wish's Summer Clothes Dataset",
+  //   description:
+  //     "Quantile analysis on Wish's Summer Clothes Dataset' on Kaggle",
+  //   link: "https://www.kaggle.com/snakepie/eda-of-wish-s-summer-clothes-sales-data",
+  //   key: "wsc",
+  //   imgFormat: "jpg",
+  // },
 ];
 
 const projectsContainer = document.querySelector(
